b4ef8d190d33d613c2006217a63e9a3bf9521f5a
unknown <jacob@grinberg.net>
2020-06-14 12:32:23 -0400

    Works on windows and changed default number of available cpu processes

--- a2c_ppo_acktr/envs.py
+++ a2c_ppo_acktr/envs.py
@@ -86,7 +86,7 @@ def make_vec_envs(env_name,
     ]
 
     if len(envs) > 1:
-        envs = ShmemVecEnv(envs, context='fork')
+        envs = ShmemVecEnv(envs)
     else:
         envs = DummyVecEnv(envs)
 
