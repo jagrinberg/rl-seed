806d3015f8540fb365abfe9e9d4476ae6abd9cd3
unknown <jacob@grinberg.net>
2020-10-26 16:55:39 -0400

    InvPend and DoublePendulum sub demos

--- bat.py
+++ bat.py
@@ -1,5 +1,13 @@
 import os
 
+os.system('python main.py --algo ppo --use-gae --num-steps 2048 --num-processes 2 --env-name "InvertedPendulumPyBulletEnv-v0" --use-linear-lr-decay --eval-interval 50 --log-dir log/subinvpend --log-interval 1 --num-env-steps 1000000')
+os.system('python create_trajectory.py --env-name "InvertedPendulumPyBulletEnv-v0" --load-dir trained_models/ppo')
+os.system('python main.py --algo ppo --use-gae --gail --gail-experts-dir gail_experts --num-steps 2048 --num-processes 2 --env-name "InvertedPendulumPyBulletEnv-v0" --use-linear-lr-decay --eval-interval 50 --log-dir log/subinvpendgail --log-interval 1 --num-env-steps 1000000')
+
+os.system('python main.py --algo ppo --use-gae --num-steps 2048 --num-processes 2 --env-name "InvertedDoublePendulumPyBulletEnv-v0" --use-linear-lr-decay --eval-interval 50 --log-dir log/subdoubpend --log-interval 1 --num-env-steps 1000000')
+os.system('python create_trajectory.py --env-name "InvertedDoublePendulumPyBulletEnv-v0" --load-dir trained_models/ppo')
+os.system('python main.py --algo ppo --use-gae --gail --gail-experts-dir gail_experts --num-steps 2048 --num-processes 2 --env-name "InvertedDoublePendulumPyBulletEnv-v0" --use-linear-lr-decay --eval-interval 50 --log-dir log/subdoubpendgail --log-interval 1 --num-env-steps 1000000')
+
 
 for i in range(0,6):
     # os.system('python main.py --algo ppo --use-gae --num-steps 512 --num-processes 2 --env-name "CartPole-v1" --use-linear-lr-decay --eval-interval 50 --log-dir log/cartpole-'+str(i)+' --log-interval 1 --seed '+str(i)+' --num-env-steps 1000000')
@@ -10,8 +18,8 @@ for i in range(0,6):
     #
    
    
-    # os.system('python mainseed.py --algo ppo --use-gae --gail --gail-experts-dir gail_experts --num-steps 2048 --num-processes 2 --env-name "InvertedDoublePendulumPyBulletEnv-v0" --use-linear-lr-decay --eval-interval 50 --log-dir log/doubinvpendseed --gail-agent-dir final_models --log-interval 1 --num-env-steps 1000000 --seed '+str(i)+' --demonstration-coef 0.03')
-    os.system('python mainseed.py --algo ppo --use-gae --gail --gail-experts-dir gail_experts --num-steps 2048 --num-processes 2 --env-name "InvertedPendulumSwingupPyBulletEnv-v0" --use-linear-lr-decay --eval-interval 50 --log-dir log/invpendswingseed --gail-agent-dir final_models --log-interval 1 --num-env-steps 1000000 --seed '+str(i)+' --demonstration-coef 0.03')
+    os.system('python mainseed.py --algo ppo --use-gae --gail --gail-experts-dir gail_experts --num-steps 2048 --num-processes 2 --env-name "InvertedDoublePendulumPyBulletEnv-v0" --use-linear-lr-decay --eval-interval 50 --log-dir log/subdoubinvpendseed --gail-agent-dir trained_models/ppo --log-interval 1 --num-env-steps 1000000 --seed '+str(i)+' --demonstration-coef 0.03')
+    os.system('python mainseed.py --algo ppo --use-gae --gail --gail-experts-dir gail_experts --num-steps 2048 --num-processes 2 --env-name "InvertedPendulumPyBulletEnv-v0" --use-linear-lr-decay --eval-interval 50 --log-dir log/subinvpendseed --gail-agent-dir trained_models/ppo --log-interval 1 --num-env-steps 1000000 --seed '+str(i)+' --demonstration-coef 0.03')
 
     # os.system('python main.py --algo ppo --use-gae --num-steps 512 --num-processes 2 --env-name "InvertedPendulumSwingupPyBulletEnv-v0" --use-linear-lr-decay --eval-interval 50 --log-dir log/invertswing-'+str(i)+' --log-interval 1 --seed '+str(i)+' --num-env-steps 1000000')
     # os.system('python main.py --algo ppo --use-gae --num-steps 2048 --num-processes 2 --env-name "InvertedDoublePendulumPyBulletEnv-v0" --use-linear-lr-decay --eval-interval 50 --log-dir log/invertdoub-'+str(i)+' --log-interval 1 --seed '+str(i)+' --num-env-steps 1000000')
